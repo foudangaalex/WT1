52322f324ac2c1f5b41640f486770f11ee72cb84
foudangaalex <foudangaalex@gmail.com>
2023-02-06 12:05:04 +0100

    third vesion

--- src/main/java/ma/enset/Test.java
+++ src/main/java/ma/enset/Test.java
@@ -6,7 +6,8 @@ public class Test{
 
 public static void main(String[] args){
 //------------------------------------
-System.out.println("hello world :)");
+String sms="Hello world";
+System.out.println(sms);
 }
 
 
